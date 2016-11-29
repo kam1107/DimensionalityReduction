95675281be051afb62f6dfa97403dc0ea8560491
kam1107 <amberxl1126@outlook.com>
2016-11-29 14:45:07 +0000

    delete general decision tree evaluation

--- drawAllEmotionTrees.m
+++
@@ -1,9 +0,0 @@
-function tree = drawAllEmotionTrees(raw_data,target_labels,emo_label)
-% This function is used to draw decision trees for
-% all the emotions with whole data set
-
-ft_lb = datatrans(raw_data,target_labels,emo_label);
-tree = ID3(ft_lb);
-DrawDecisionTree(tree,strcat('Emotion',num2str(emo_label)));
-
-
